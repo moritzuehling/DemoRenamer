4e712d8c7ffc927403de468b625b13543f331006
moritzuehling <github@moritzuehling.de>
2014-11-01 12:25:47 +0100

    Inital Commit

---
+++ DemoRenamer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRenamer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			foreach(var file in args)
+			{
+
+			}
+		}
+	}
+}
